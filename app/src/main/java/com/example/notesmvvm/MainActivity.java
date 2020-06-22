96f924401a0b4b7a16354f83702cfc40d419f640
Shashank Sinha <34626597+ShashankSinha98@users.noreply.github.com>
2020-06-22 19:05:20 +0530

    Completed Repository - Part 8

--- app/src/main/java/com/example/notesmvvm/MainActivity.java
+++ app/src/main/java/com/example/notesmvvm/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.notesmvvm;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.List;
+
+import es.dmoral.toasty.Toasty;
 
 
 /*
@@ -18,11 +30,29 @@ import java.util.List;
 * */
 public class MainActivity extends AppCompatActivity {
 
+    //psfi
+    public static final int ADD_NOTE_REQUEST = 1;
+
     private NoteViewModel noteViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FloatingActionButton buttonAddNNote = findViewById(R.id.button_add_note);
+        buttonAddNNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,AddNoteActivity.class);
+
+                // To get I/p back to MainActivity and add it to DB. MainActivity has reference to ViewModel.
+                startActivityForResult(intent,ADD_NOTE_REQUEST); // request code is needed bcz startActivityForResult can be called multiple
+                                                                // times from MainActivity from other parts. So to distinguish them, we need it.
+            }
+        });
+
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -51,5 +81,75 @@ public class MainActivity extends AppCompatActivity {
                 adapter.setNotes(notes);
             }
         });
+
+
+        // Swipe To Delete Functionality, ItemTouchHelper - make our Recycler View Swipable
+        // ItemTouchHelper.SimpleCallback(__,__) - need to know direction of movement,
+        // 1st arg is for drag and drop, Since we are not implementing it, thus - 0
+        // 2nd arg is for swiping, we are adding both left and right.
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+            // onMove is drag and drop functionality
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+                // viewHolder.getAdapterPosition() - tells which item you swiped, position
+                noteViewModel.delete(adapter.getNoteAt(viewHolder.getAdapterPosition()));
+                Toasty.success(MainActivity.this, "Note Deleted !",Toasty.LENGTH_SHORT).show();
+            }
+        }).attachToRecyclerView(recyclerView);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // checking if we get RESULT_OK from ADD_NOTE_REQUEST Activity, then it contains data
+        if(requestCode == ADD_NOTE_REQUEST && resultCode == RESULT_OK){
+
+            String title = data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String description = data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+            int priority = data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY,1);
+
+            Note note = new Note(title,description,priority);
+            noteViewModel.insert(note);
+
+            Toasty.success(this,"Note Saved",Toasty.LENGTH_LONG).show();
+
+        } else {
+            // If resultCode == RESULT_CANCEL, user pressed back in AddNoteActivity
+            Toasty.warning(this,"Note not saved !",Toasty.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.delete_all_notes:
+                noteViewModel.deleteAllNotes();
+                Toasty.success(MainActivity.this,"All notes deleted",Toasty.LENGTH_SHORT).show();
+                return  true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
